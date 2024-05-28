4837dc1b8326e82a5288dd8089aecfc2f4e5acb9
ibtisam-adil <ibtisamadil273@gmail.com>
2024-05-28 07:06:00 +0500

    Changed the files according to vite format

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import './App.css';
 import SignIn from './components/signPage/SignIn';
 import SignUp from './components/signPage/SignUp';
 
-function App() {
-  return (
-    <div className="App">
-      <SignIn />
-      <SignUp />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <SignIn />
+    <SignUp />
+  </div>
+);
 
 export default App;
