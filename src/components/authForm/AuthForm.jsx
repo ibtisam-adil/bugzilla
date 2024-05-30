89c8c32938162d44acbbcebd6ad900c67c48d413
ibtisam-adil <ibtisamadil273@gmail.com>
2024-05-30 10:20:10 +0500

    setup redux

--- src/components/authForm/AuthForm.jsx
+++ src/components/authForm/AuthForm.jsx
@@ -11,13 +11,13 @@ const AuthForm = ({
   welcomeMsg = '',
   welcomeDetail = '',
 }) => (
-  <div className="main">
-    <div className="auth-page">
-      <div className="auth-container">
-        <div className="auth-header">
+  <div className="flex justify-center items-center h-screen">
+    <div className="grid place-items-center grid-cols-1 md:grid-cols-custom font-roboto text-3xl w-4/5 h-3/4 bg-white rounded-3xl shadow-[0_5px_20px_rgba(77,91,170,0.5)]">
+      <div className="flex flex-col justify-center items-center text-center w-4/5 mx-auto">
+        <div className="mb-12">
           <h2>{title}</h2>
           {title === 'Hello!' && (
-            <p className="signin-desc">Sign in to your account</p>
+            <p className="text-gray-600">Sign in to your account</p>
           )}
         </div>
         <form className="auth-form" action="/login" method="post">
@@ -28,20 +28,20 @@ const AuthForm = ({
             </div>
           ))}
           {showCheckbox && (
-            <div className="checkbox-container">
-              <label htmlFor="agreeTerms" className="terms-conditions-label">
+            <div className="text-base">
+              <label htmlFor="agreeTerms" className="flex justify-center">
                 <input type="checkbox" id="agreeTerms" name="agreeTerms" required />
-                <p className="term">I read and agree to the Terms and Conditions</p>
+                <p className="ml-4 text-blue-500 text-1xl">I read and agree to the Terms and Conditions</p>
               </label>
             </div>
           )}
-          <div className="signin-button-container">
+          <div>
             <input type="submit" value={buttonText} />
           </div>
         </form>
-        <p className="auth-message">{message}</p>
+        <p className="text-blue-500 text-2xl">{message}</p>
       </div>
-      <div className="welcomeback-section">
+      <div className="bg-gradient-to-r from-custom-red to-custom-pink flex flex-col justify-center items-center h-full text-white rounded-tr-2xl rounded-br-2xl text-center gap-8 p-20 lg:p-16 md:p-8 max-md:hidden">
         <h3>{welcomeMsg}</h3>
         <p>{welcomeDetail}</p>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'max-md': { max: '768px' },
+      },
+      colors: {
+        'custom-red': '#ff4d4d',
+        'custom-pink': '#f99',
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+      },
+      boxShadow: {
+        custom: '0 5px 20px rgba(77, 91, 170, 0.5)',
+      },
+      gridTemplateColumns: {
+        custom: '1.4fr 1fr',
+      },
+    },
   },
   plugins: [],
 };
